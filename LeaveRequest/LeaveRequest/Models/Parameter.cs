474fa45ad65c4b7d155ba2a27243cb84f9fad5cf
Hafidz Naufal <m.hafidznaufalhilal@gmail.com>
2021-05-26 13:47:21 +0700

    Third

---
+++ LeaveRequest/LeaveRequest/Models/Parameter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LeaveRequest.Models
+{
+    [Table("TB_M_Parameter")]
+    public class Parameter
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Value { get; set; }
+        public string Notes { get; set; }
+    }
+}
--- LeaveRequest/LeaveRequest/Models/Request.cs
+++ LeaveRequest/LeaveRequest/Models/Request.cs
@@ -1,11 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace LeaveRequest.Models
 {
+    [Table("TB_T_Request")]
     public class Request
     {
+        [Key]
+        public int Id { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string ReasonRequest { get; set; }
+        public string NIK_Employee { get; set; }
+        public string Notes { get; set; }
+        public enum StatusRequest { get; set; }
     }
 }
