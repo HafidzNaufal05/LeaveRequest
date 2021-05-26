75d1195f938feb54f355b8be5d1f97bfdf3a8881
Hafidz Naufal <61229724+HafidzNaufal05@users.noreply.github.com>
2021-05-26 12:48:23 +0700

    Secondary

---
+++ LeaveRequest/LeaveRequest/Context/MyContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LeaveRequest.Context
+{
+    public class MyContext : DbContext
+    {
+        public MyContext()
+        {
+
+        }
+        public MyContext(DbContextOptions<MyContext>options): base(options)
+        {
+
+        }
+    }
+}
