5e9b0dfae3a670e3522e482fb26fb78507f13acd
Khanzarg <khanzarahmasilla@gmail.com>
2021-05-27 00:12:38 +0700

    undo

---
+++ LeaveRequest/LeaveRequest/ViewModels/RegisterVM.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LeaveRequest.ViewModels
+{
+    public class RegisterVM
+    {
+        public string NIK { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Gender { get; set; }
+        public string Address { get; set; }
+        public string MaritialStatus { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public DateTime JoinDate { get; set; }
+        public string DepartmentId { get; set; }
+        public string RoleId { get; set; }
+        public string Password { get; set; }
+    }
+}
