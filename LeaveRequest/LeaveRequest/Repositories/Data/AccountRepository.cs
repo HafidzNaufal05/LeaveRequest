39e9faa0da0ef551bced6257b676598853cf1835
Khanzarg <khanzarahmasilla@gmail.com>
2021-05-26 13:53:49 +0700

    merge of "Khanza" and "origin/main"

---
+++ LeaveRequest/LeaveRequest/Repositories/Data/AccountRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LeaveRequest.Repositories.Data
+{
+    public class AccountRepository
+    {
+    }
+}
---
+++ LeaveRequest/LeaveRequest/Repositories/Interfaces/IGenericRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LeaveRequest.Repositories.Interfaces
+{
+    public interface IGenericRepository<Entity, Id> where Entity : class
+    {
+        IEnumerable<Entity> GetAll();
+        Entity GetById(Id id);
+        int Create(Entity entity);
+        int Update(Entity entity);
+        int Delete(Id id);
+    }
+}
